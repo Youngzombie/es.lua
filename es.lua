b2f39a203b2c4eeeca43bfdcf22c615c9d3f287e
Youngzombie <71993687+Youngzombie@users.noreply.github.com>
2021-05-17 22:17:00 +0200

    Add files via upload

---
+++ es.lua
@@ -0,0 +1,13 @@
+Locales ['es'] = {
+  ['buy_license'] = '¿Comprar licencia de armas?',
+  ['yes'] = '%s',
+  ['no'] = 'no',
+  ['weapon_bought'] = 'Comprar por %s €',
+  ['not_enough_black'] = 'No tienes ~r~suficiente~s~ dinero negro',
+  ['not_enough'] = 'No tienes ~r~suficiente~s~ dinero',
+  ['already_owned'] = 'Ya tienes esta arma!',
+  ['shop_menu_title'] = 'Tienda',
+  ['shop_menu_prompt'] = 'Presiona ~INPUT_CONTEXT~ para acceder a la tienda',
+  ['shop_menu_item'] = '%s €',
+  ['map_blip'] = 'Tienda de Armas',
+}
